dc288f2e36aa0efc12eeac3865260fc44063a7df
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-05-02 00:14:43 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({ credentials: true }));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
